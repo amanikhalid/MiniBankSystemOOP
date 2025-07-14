2d481900edf1d7931b682809203ae6db4459ec01
Amani Khalid <amanikhalidd@gmail.com>
2025-07-14 14:52:04 +0400

    Added Account Properties

--- Account.cs
+++ Account.cs
@@ -8,5 +8,15 @@ namespace MiniBankSystemOOP
 {
     class Account
     {
+        public int AccountNumber { get; private set; }
+        public string Name { get; private set; }
+        public string NationalId { get; private set; }
+        public double Balance { get; private set; }
+        public string UserType { get; set; }
+        public bool IsLocked { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        private string HashedPassword;
+
     }
 }
