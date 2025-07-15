8ee1e5425e2e852e2a70bba9b11d9041a8589e7c
Amani Khalid <amanikhalidd@gmail.com>
2025-07-15 15:05:06 +0400

    Added Transaction Function

--- Transaction.cs
+++ Transaction.cs
@@ -12,5 +12,20 @@ namespace MiniBankSystemOOP
         public double Amount { get; set; } 
         public double BalanceAfter { get; set; }
         public DateTime Timestamp { get; set; }
+
+        public Transaction(string type, double amount, double balanceAfter)
+        {
+            Type = type;
+            Amount = amount;
+            BalanceAfter = balanceAfter;
+            Timestamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{Timestamp:yyyy-MM-dd HH:mm} - {Type} {Amount:F2} OMR | Balance: {BalanceAfter:F2}";
+        }
     }
+
+
 }
