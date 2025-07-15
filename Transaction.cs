db1ad1afd36c66417985e61c1bd17c9c602823c0
Amani Khalid <amanikhalidd@gmail.com>
2025-07-15 15:03:45 +0400

    Inserted Transaction Fields

---
+++ Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBankSystemOOP
+{
+    class Transaction
+    {
+        public string Type { get; set; } 
+        public double Amount { get; set; } 
+        public double BalanceAfter { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
