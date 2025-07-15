92f4074eb5fe290a23273c1ea2db64008643cd05
Amani Khalid <amanikhalidd@gmail.com>
2025-07-15 14:49:07 +0400

    Inserted Review Fields

---
+++ Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBankSystemOOP
+{
+    class Review
+    {
+        public int AccountNumber { get; set; }
+        public string Comment { get; set; }
+        public DateTime Timestamp { get; set; }
+
+    }
+}
